079a904d3d3507c280fc5ba5b8a1e336c59c16aa
kurwanator1337 <ktvcss@mail.ru>
2024-01-06 02:32:22 +0400

    Добавьте файлы проекта.

---
+++ kTVCSS-Web/wwwroot/js/main.js
@@ -0,0 +1,65 @@
+﻿var uid;
+
+function redirect(url) {
+    let ua = navigator.userAgent.toLowerCase(),
+        isIE = ua.indexOf('msie') !== -1,
+        version = parseInt(ua.substr(4, 2), 10);
+
+    if (isIE && version < 9) {
+        let link = document.createElement('a');
+        link.href = url;
+        document.body.appendChild(link);
+        link.click();
+    }
+
+    else {
+        window.location.href = url;
+    }
+}
+
+function changeUrl(url) {
+    window.history.pushState("state", "title", url);
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function httpGet(url) {
+    return new Promise(function (resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onload = function () {
+            if (this.status == 200) {
+                resolve(this.response);
+            } else {
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(new Error("Network Error"));
+        };
+
+        xhr.send();
+    });
+}

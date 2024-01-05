163953a0da76b70cf774bc91f80c65f54e8e5a0b
kurwanator1337 <ktvcss@mail.ru>
2024-01-06 02:49:21 +0400

    Added config file

---
+++ kTVCSS-Web/Models/WebConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace kTVCSS.Models
+{
+    public class WebConfig
+    {
+        /// <summary>
+        /// Application config file
+        /// </summary>
+        public static WebConfig Instance { get; set; } = new WebConfig();
+
+        /// <summary>
+        /// DB connection string
+        /// </summary>
+        public string DbConnectionString { get; set; }
+    }
+}
--- kTVCSS-Web/Program.cs
+++ kTVCSS-Web/Program.cs
@@ -1,25 +1,77 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using kTVCSS;
+using kTVCSS.Controllers;
+using kTVCSS.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace kTVCSS
+internal class Program
 {
-    public class Program
+    private static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // add config
+        builder.Configuration.AddJsonFile("config.json");
+
+        builder.Services.AddMemoryCache();
+
+        builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+        builder.Services.AddAuthentication("kTVCSSv1")
+               .AddCookie("kTVCSSv1", options =>
+               {
+                   options.AccessDeniedPath = "/NoAccess";
+                   options.LoginPath = new PathString("/api/auth2");
+                   options.Cookie.MaxAge = new TimeSpan(365, 1, 1, 1);
+               });
+
+        builder.Services.AddAuthorization(opts => {
+
+        });
+
+        var app = builder.Build();
+
+        #region bind and show config
+
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        app.Configuration.Bind(WebConfig.Instance);
+        Console.WriteLine($"Database: {WebConfig.Instance.DbConnectionString}");
+
+        Console.ForegroundColor = ConsoleColor.Gray;
+
+        #endregion
+
+        app.Environment.EnvironmentName = "Development";
+
+        if (!app.Environment.IsDevelopment())
         {
-            CreateHostBuilder(args).Build().Run();
+            app.UseExceptionHandler("/Home/Error");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
